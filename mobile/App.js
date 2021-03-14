619f24534fe4b1a531981893954d3396326d31db
Wenwei Tao <wenweitao@wenweis-mbp.lan>
2021-03-13 18:02:29 -0800

    Initial mobile code

---
+++ mobile/App.js
@@ -0,0 +1,61 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, useColorScheme} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from './screens/home/Home';
+import DetailScreen from './screens/details/Detail';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {Provider as PaperProvider} from 'react-native-paper';
+import AppStore from './screens/appStore';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <AppStore.AppContextProvider>
+      <PaperProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Details" component={DetailScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </AppStore.AppContextProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ mobile/screens/home/Home.js
@@ -0,0 +1,114 @@
+import * as React from 'react';
+import {View, StyleSheet, SafeAreaView, FlatList} from 'react-native';
+import HomeStore from './HomeStore';
+import {useNavigation} from '@react-navigation/native';
+import {
+  Searchbar,
+  Button,
+  IconButton,
+  Card,
+  Paragraph,
+  Colors,
+} from 'react-native-paper';
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
+import AppStore from '../appStore';
+
+const HomeScreenRoot = ({props}) => {
+  const navigation = useNavigation();
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const {pathogens, refresh} = HomeStore.useHomeStoreContext();
+  const {dirty, setDirty} = AppStore.useAppContext();
+
+  React.useEffect(() => {
+    if (dirty) {
+      refresh('name=_');
+    }
+  }, [dirty]);
+
+  React.useEffect(() => {
+    console.log(pathogens);
+    setDirty(false);
+  }, [pathogens]);
+
+  const onChangeSearch = query => setSearchQuery(query);
+
+  return (
+    <SafeAreaView>
+      <View style={{height: '100%'}}>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+        <FlatList
+          data={pathogens}
+          renderItem={({item}) => {
+            return (
+              <Card>
+                <Card.Title title={item.name} subtitle={item.scientificName} />
+                <Card.Content>
+                  <Paragraph>{item.family}</Paragraph>
+                  <Paragraph>{item.ClinicalSymptoms}</Paragraph>
+                </Card.Content>
+                <Card.Actions>
+                  <Button
+                    onPress={() => {
+                      navigation.navigate('Details', {id: item.id, edit: true});
+                    }}>
+                    Edit
+                  </Button>
+                </Card.Actions>
+              </Card>
+            );
+          }}
+          keyExtractor={item => item.id}
+        />
+      </View>
+      <ActionButton buttonColor="rgba(231,76,60,1)">
+        <ActionButton.Item
+          buttonColor="#9b59b6"
+          title="New Pathogen"
+          onPress={() => {
+            navigation.navigate('Details', {edit: false});
+          }}>
+          <Icon name="md-create" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item
+          buttonColor="#1abc9c"
+          title="Search by Genome"
+          onPress={() => {}}>
+          <Icon name="md-search" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item
+          buttonColor="#3498db"
+          title="Refresh"
+          onPress={() => setDirty(true)}>
+          <Icon name="md-refresh" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+      </ActionButton>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen = ({props, navigation}) => {
+  return (
+    <HomeStore.HomeContextProvider>
+      <HomeScreenRoot />
+    </HomeStore.HomeContextProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'whitesmoke',
+  },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
+});
