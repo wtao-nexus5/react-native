933d17e2246d2a856a583a750edaccd820669356
Wenwei Tao <wenweitao@wenweis-mbp.lan>
2021-03-19 14:49:32 -0700

    Unit test with report

--- common/src/__tests__/AppStore.test.js
+++ common/src/__tests__/AppStore.test.js
@@ -1,22 +1,48 @@
 import * as React from 'react'
-import { renderHook } from '@testing-library/react-hooks'
-import { useAppContext } from '../AppStore'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import AppStore from '../AppStore'
 
-describes('AppStore', () => {
+Enzyme.configure({ adapter: new Adapter() })
+
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: jest.fn()
+}))
+
+describe('AppStore', () => {
   it('provides', () => {
-    let spy = jest.spyOn(React, 'useState')
-
-    spy.mockImplementationOnce((value) => [[{ id: 0 }], jest.fn()])
-    spy.mockImplementationOnce((value) => [0, jest.fn()])
-    spy.mockImplementationOnce((value) => [1, jest.fn()])
-    spy.mockImplementationOnce((value) => [true, jest.fn()])
-    spy.mockImplementationOnce((value) => ['error', jest.fn()])
-    spy.mockImplementationOnce((value) => [false, jest.fn()])
-    spy.mockImplementationOnce((value) => [true, jest.fn()])
-    spy.mockImplementationOnce((value) => [false, jest.fn()])
-    spy.mockImplementationOnce((value) => ['HPV', jest.fn()])
-
-    const result = renderHook(() => useAppContext())
-    expect(result.pathogens.length).toBe(1)
+    const useStateSpy = jest.spyOn(React, 'useState')
+    useStateSpy.mockImplementation((_) => [null, jest.fn()])
+    const wrapper = shallow(<AppStore.AppContextProvider />)
+    const keys = Object.keys(wrapper.props().value)
+    const fieldNames = [
+      'pathogens',
+      'setPathogens',
+      'modeEnum',
+      'currentPid',
+      'setCurrentPid',
+      'editMode',
+      'setEditMode',
+      'showError',
+      'setShowError',
+      'errorMsg',
+      'setErrorMsg',
+      'busy',
+      'setBusy',
+      'landscape',
+      'setLandscape',
+      'restApi',
+      'safeApiCall',
+      'searchQuery',
+      'setSearchQuery',
+      'dirty',
+      'setDirty'
+    ]
+    const allMatch = keys.reduce(
+      (accum, key) => accum && (fieldNames.indexOf(key) > -1 ? true : false),
+      true
+    );
+    expect(allMatch).toBe(true)
   })
 })
